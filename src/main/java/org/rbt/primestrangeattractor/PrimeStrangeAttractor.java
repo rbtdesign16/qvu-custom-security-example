9efcc618b66189a5eb710bb83825a1b6c5ba04bf
rbtucker <rbtucker16@gmail.com>
2025-04-15 13:08:45 -0500

    design/develop

--- src/main/java/org/rbt/primestrangeattractor/PrimeStrangeAttractor.java
+++ src/main/java/org/rbt/primestrangeattractor/PrimeStrangeAttractor.java
@@ -9,15 +9,13 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class PrimeStrangeAttractor {
-    private static final int CANVAS_SIZE = 1200;
-    private static final int CANVAS_ZOOM = 50;
+    private static final int CANVAS_SIZE = 16000;
+    private static final int CANVAS_ZOOM = 5;
     private static final int PRIMES_PER_LINE = 1000;
-    private static final int DIFFS_PER_LINE = 20;
-    private static final int MAX_PRIME_COUNT = 1000000;
+    private static final int MAX_PRIME_COUNT = 500000;
     private static final String CANVAS_BACK_COLOR = "black";
     private static final String OUTPUT_FILE = "/Users/rbtuc/Desktop/psa-graph.html";
     private static final String PRIME_FILE = "C:\\dev\\projects\\primestrangeattractor\\src\\main\\java\\org\\rbt\\primestrangeattractor\\primes.txt";
-    private static final String DIFF_FILE = "C:\\dev\\projects\\primestrangeattractor\\src\\main\\java\\org\\rbt\\primestrangeattractor\\diff.csv";
 
     public static void main(String[] args) {
         printHtml();
@@ -52,31 +50,6 @@ public class PrimeStrangeAttractor {
         return retval;
     }
 
-    private static List<Integer> loadDiffs() {
-        List<Integer> retval = new ArrayList<>();
-        LineNumberReader lnr = null;
-        try {
-            int indx = 0;
-            lnr = new LineNumberReader(new FileReader(DIFF_FILE));
-            String line;
-            while ((line = lnr.readLine()) != null) {
-                indx++;
-                if (StringUtils.isNotEmpty(line)) {
-                    int pos = line.indexOf(",");
-                    retval.add(Integer.parseInt(line.substring(0, pos).trim()));
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                lnr.close();
-            } catch (Exception ex) {
-            };
-        }
-
-        return retval;
-    }
 
     private static void printHtml() {
         PrintWriter pw = null;
@@ -86,16 +59,15 @@ public class PrimeStrangeAttractor {
             pw.println("<html>");
             printScriptTag(pw);
             pw.println("<body>");
-            pw.println("\t<button onClick='doGraph(true)'>Prev</button><button onClick='doGraph()'>Next</button><br />");
             pw.println("\t<canvas id='sp' width='"
                     + CANVAS_SIZE
                     + "' height='"
                     + CANVAS_SIZE
-                    + "' style='border:1px solid darkgray; background-color: "
+                    + "' style='background-color: "
                     + CANVAS_BACK_COLOR
                     + "; zoom: "
                     + CANVAS_ZOOM + "%'></canvas>");
-            pw.println("\t<script type='text/javascript'></script>");
+            pw.println("\t<script type='text/javascript'>doGraph();</script>");
             pw.println("</body></html>");
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -109,34 +81,25 @@ public class PrimeStrangeAttractor {
 
     private static void printScriptTag(PrintWriter pw) throws Exception {
         pw.println("<script type='text/javascript'>");
-        pw.println("\tconst diffs = " + buildArray(loadDiffs(), DIFFS_PER_LINE, false) + ";");
         pw.println("\tconst primes = " + buildArray(loadPrimes(), PRIMES_PER_LINE, false) + ";");
         pw.println();
 
         pw.println("\tconst CANVAS_SIZE = " + CANVAS_SIZE + ";");
-        pw.println("\tconst POINT_MODULUS = 0;");
         pw.println("\tconst AXIS_COLOR = 'crimson';");
-        pw.println("\tconst AXIS_WIDTH = 1;");
-        pw.println("\tconst POINT_SIZE = 2;");
+        pw.println("\tconst AXIS_WIDTH = 20;");
+        pw.println("\tconst POINT_SIZE = 10;");
         pw.println("\tconst centerX = CANVAS_SIZE / 2;");
         pw.println("\tconst centerY = CANVAS_SIZE / 2;");
-        pw.println("\tconst DEFAULT_ALPHA = 0.08;");
+        pw.println("\tconst DEFAULT_ALPHA = 0.05;");
         pw.println("\tconst DEFAULT_POINT_COLOR = 'cornsilk';");
         pw.println("\tconst MAX_PRIME_LIMIT = primes[primes.length - 1] + 100;");
-        pw.println("\t let diffindx = 1;");
         pw.println();
-        pw.println("\tasync function doGraph(prev) {");
+        pw.println("\tasync function doGraph() {");
         pw.println("\t\tlet ctx = document.getElementById('sp').getContext('2d');");
-        pw.println();
-        pw.println("\t\tctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);");
-        pw.println("\t\tif ((diffindx < diffs.length) && (diffindx > 0)) {;");
-        pw.println("\t\t\tif (prev) { diffindx--; };");
         pw.println("\t\t\tdrawAxis(ctx);");
         pw.println("\t\t\tfor (let primeindx = 0; primeindx < primes.length; ++primeindx) {");
-        pw.println("\t\t\t\tdrawPoint(ctx, diffs[diffindx], primeindx);");
+        pw.println("\t\t\t\tdrawPoint(ctx, primeindx);");
         pw.println("\t\t\t}");
-        pw.println("\t\t\tif (!prev) { diffindx++; };");
-        pw.println("\t\t} else { alert('end of diff array');}");
         pw.println("\t}");
         pw.println();
 
@@ -154,29 +117,17 @@ public class PrimeStrangeAttractor {
         pw.println("\t};");
         pw.println();
 
-        pw.println("\tfunction drawPoint(ctx, diff, primeindx) {");
-        pw.println("\t\tif ((primes[primeindx] - primes[primeindx - 1]) === diff) {");
+        pw.println("\tfunction drawPoint(ctx, primeindx) {");
         pw.println("\t\t\tctx.beginPath();");
         pw.println("\t\t\tlet scale = (primes[primeindx] / primes[primes.length - 1]);");
         pw.println("\t\t\tlet theta =  scale * Math.PI * 2;");
         pw.println("\t\t\tlet offset =  scale * centerX;");
         pw.println("\t\t\tlet x = centerX + offset * Math.cos(theta); ");
         pw.println("\t\t\tlet y = centerY + offset * Math.sin(theta);");
-        /*
         pw.println("\t\t\tctx.globalAlpha = DEFAULT_ALPHA;");
         pw.println("\t\t\tctx.fillStyle = DEFAULT_POINT_COLOR");
-        pw.println("\t\t\tctx.arc(x, y, POINT_SIZE, 0, Math.PI * 2);");
+        pw.println("\t\t\tctx.arc(x, y, POINT_SIZE, POINT_SIZE, Math.PI * 2);");
         pw.println("\t\t\tctx.fill();");
-*/
-        pw.println("\t\t\tctx.beginPath();");
-        pw.println("\t\t\tctx.strokeStyle = DEFAULT_POINT_COLOR;");
-        pw.println("\t\t\tctx.globalAlpha = DEFAULT_ALPHA;");
-        pw.println("\t\t\tctx.lineWidth = 1;");
-        pw.println("\t\t\tctx.moveTo(centerX, centerY);");
-        pw.println("\t\t\tctx.lineTo(x, y);");
-        pw.println("\t\t\tctx.stroke();");
-
-        pw.println("\t\t};");
         pw.println("\t};");
         pw.println("</script>");
         pw.println();
